4b1e66dbabca71d4bf5427588e1a5b743a35fcad
SB <biggsst@gmail.com>
2015-05-02 15:08:51 -0400

    Share

--- app/src/main/java/com/qtrandev/arex/ArexMainMenuActivity.java
+++ app/src/main/java/com/qtrandev/arex/ArexMainMenuActivity.java
@@ -1,17 +1,118 @@
 package com.qtrandev.arex;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookGraphResponseException;
+import com.facebook.login.DefaultAudience;
+import com.facebook.login.LoginManager;
+import com.facebook.share.Sharer;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareButton;
+import com.facebook.share.widget.ShareDialog;
+
+import java.util.Arrays;
 
 
 public class ArexMainMenuActivity extends ActionBarActivity {
+    private ShareButton shareButton;
+    private CallbackManager callbackManager;
+    private AccessTokenTracker accessTokenTracker;
+    private ShareDialog shareDialog;
+
+
+
+    private FacebookCallback<Sharer.Result> shareCallback =
+            new FacebookCallback<Sharer.Result>() {
+                @Override
+                public void onCancel() {
+                    Toast.makeText(ArexMainMenuActivity.this, "Cancelled", Toast.LENGTH_SHORT).show();
+
+                }
+
+                @Override
+                public void onError(FacebookException error) {
+                    if (error instanceof FacebookGraphResponseException) {
+                        FacebookGraphResponseException graphError =
+                                (FacebookGraphResponseException) error;
+                        if (graphError.getGraphResponse() != null) {
+                            Toast.makeText(ArexMainMenuActivity.this, "Graph Error", Toast.LENGTH_SHORT).show();
+
+                            return;
+                        }
+                    }
+                    Toast.makeText(ArexMainMenuActivity.this, "Unknown Error", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onSuccess(Sharer.Result result) {
+                    Toast.makeText(ArexMainMenuActivity.this, "Worked", Toast.LENGTH_SHORT).show();
+
+                }
+            };
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arex_main_menu);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        shareButton = (ShareButton)findViewById(R.id.share_button);
+        shareDialog = new ShareDialog(this);
+        // this part is optional
+        shareDialog.registerCallback(callbackManager, shareCallback);
+
+          accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken,
+                                                       AccessToken currentAccessToken) {
+                updateWithToken(currentAccessToken);
+            }
+        };
+    }
+
+
+
+    private void updateWithToken(AccessToken currentAccessToken) {
+        if (currentAccessToken != null) {
+            Toast.makeText(ArexMainMenuActivity.this, "new token", Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(ArexMainMenuActivity.this, "error", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    public void shareClick(View view){
+
+        if (ShareDialog.canShow(ShareLinkContent.class)) {
+            ShareLinkContent linkContent = new ShareLinkContent.Builder()
+                    .setContentTitle("Hello Facebook")
+                    .setContentDescription(
+                            "[Description]")
+                    .setContentUrl(Uri.parse("http://developers.facebook.com/android"))
+                    .build();
+
+            shareDialog.show(linkContent);
+        }
     }
 
 
@@ -20,6 +121,12 @@ public class ArexMainMenuActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_arex_main_menu, menu);
         return true;
+    }
+
+    private void requestPublishPermissions() {
+        LoginManager.getInstance()
+                .setDefaultAudience(DefaultAudience.FRIENDS)
+                .logInWithPublishPermissions(this, Arrays.asList("publish_actions"));
     }
 
     @Override
