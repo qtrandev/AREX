36c6543ccf7ab09ff2151f01fb294a67387aef04
SB <biggsst@gmail.com>
2015-05-02 21:25:01 -0400

    CameraPhoto

--- app/src/main/java/com/qtrandev/arex/ArexMainMenuActivity.java
+++ app/src/main/java/com/qtrandev/arex/ArexMainMenuActivity.java
@@ -1,11 +1,14 @@
 package com.qtrandev.arex;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -28,10 +31,9 @@ import com.facebook.share.widget.ShareButton;
 import com.facebook.share.widget.ShareDialog;
 
 import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
+import java.io.FileOutputStream;
 import java.util.Arrays;
-import java.util.Date;
+import java.util.Random;
 
 
 public class ArexMainMenuActivity extends ActionBarActivity {
@@ -42,11 +44,20 @@ public class ArexMainMenuActivity extends ActionBarActivity {
     private ShareDialog shareDialog;
     private String mCurrentPhotoPath;
     private boolean takingPhoto = false;
-    File photoFile=null;
-
+    private File photoFile=null;
+    static final int CAMERA_PIC_REQUEST = 1;
     private static final int REQUEST_CODE_SHARE_TO_MESSENGER = 1;
     static final int REQUEST_IMAGE_CAPTURE = 1;
     static final int REQUEST_TAKE_PHOTO = 1;
+
+
+    //Info for photo intent
+
+    private static final String EXTRA_PROTOCOL_VERSION = "com.facebook.orca.extra.PROTOCOL_VERSION";
+    private static final String EXTRA_APP_ID = "com.facebook.orca.extra.APPLICATION_ID";
+    private static final int PROTOCOL_VERSION = 20150314;
+    private static final String YOUR_APP_ID = "870248279704331";
+    private static final int SHARE_TO_MESSENGER_REQUEST_CODE = 1;
 
 
     private FacebookCallback<Sharer.Result> shareCallback =
@@ -80,90 +91,88 @@ public class ArexMainMenuActivity extends ActionBarActivity {
 
     @Override
     protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (takingPhoto) {
-            processPhotoData(data);//Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
-            takingPhoto = false;
-        } else {
-            callbackManager.onActivityResult(requestCode, resultCode, data);
+        if(takingPhoto){
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            this.takingPhoto=false;
+            processPhotoDataForMessenger(photo);
         }
+        else
+        {
+            super.onActivityResult(requestCode, resultCode, data);
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+
+        }
+
     }
-    /*
+
+
     public void dispatchTakePictureIntent(View view) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            this.takingPhoto=true;
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
-    */
 
-    private File createImageFile() throws IOException {
-        // Create an image file name
+/*
+    private void processPhotoDataForWallPost(Bitmap photo) {
 
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
 
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
-        return image;
-    }
 
-    public void dispatchTakePictureIntent(View view) {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
 
-            photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
+        if (ShareDialog.canShow(ShareLinkContent.class)) {
 
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                takingPhoto = true;
-                //takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
-                        //Uri.fromFile(photoFile));
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-            }
+            SharePhoto sharePhoto = new SharePhoto.Builder()
+                    .setBitmap(pho)
+                    .build();
+
+            SharePhotoContent photoContent = new SharePhotoContent.Builder()
+                    .addPhoto(photo)
+                    .build();
+
+            shareDialog.show(photoContent);
         }
     }
 
-    private void processPhotoData(Intent data) {
-        Uri cameraPhotoUri = Uri.fromFile(photoFile);
+*/
+    public Uri bitmapToUriConverter(Bitmap mBitmap) {
+        Uri uri = null;
+        try {
+            final BitmapFactory.Options options = new BitmapFactory.Options();
 
 
-        // contentUri points to the content being shared to Messenger
+            File file = new File(this.getFilesDir(), "Image"
+                    + new Random().nextInt() + ".jpeg");
+            FileOutputStream out = this.openFileOutput(file.getName(),
+                    Context.MODE_WORLD_READABLE);
+            mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+            String realPath = file.getAbsolutePath();
+            File f = new File(realPath);
+            uri = Uri.fromFile(f);
+
+        } catch (Exception e) {
+            Log.e("Cannot convert to uri", e.getMessage());
+        }
+        return uri;
+    }
+
+    public void processPhotoDataForMessenger(Bitmap photo) {
+
+
         ShareToMessengerParams shareToMessengerParams =
-                ShareToMessengerParams.newBuilder(cameraPhotoUri, "image/jpeg").build();
-
-        //.setMetaData("{ \"image\" : \"camera_photo\" }")
+                ShareToMessengerParams.newBuilder(this.bitmapToUriConverter(photo), "image/jpeg").build();
 
 
-
-        // Sharing from an Activity
         MessengerUtils.shareToMessenger(
                 this,
                 REQUEST_CODE_SHARE_TO_MESSENGER,
                 shareToMessengerParams);
     }
 
-    //Save to internal storage
-    //figure out how to integrate
-    private void galleryAddPic() {
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        File f = new File(mCurrentPhotoPath);
-        Uri contentUri = Uri.fromFile(f);
-        mediaScanIntent.setData(contentUri);
-        this.sendBroadcast(mediaScanIntent);
-    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -178,7 +187,7 @@ public class ArexMainMenuActivity extends ActionBarActivity {
         // this part is optional
         shareDialog.registerCallback(callbackManager, shareCallback);
 
-          accessTokenTracker = new AccessTokenTracker() {
+        accessTokenTracker = new AccessTokenTracker() {
             @Override
             protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken,
                                                        AccessToken currentAccessToken) {
